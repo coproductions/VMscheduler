e908e66ace6dc5fdcb225dfc1d761ce22936359c
coproductions <constantin.dumba@gmail.com>
2016-03-03 12:07:05 -0800

    trying out my scheduler

--- myTest.lua
+++ myTest.lua
@@ -28,31 +28,31 @@ end
 
 local function task1()
   print("first task, first line")
-  myScheduler:yield();
+  yield();
   print("first task, second line")
 end
 
 local function task2()
   print("second task, only line")
-  myScheduler:yield();
+  yield();
 end
 
 local function counter(name, nCount)
   for num in numbers(nCount) do
     print(name, num);
-  myScheduler:yield();
+    yield();
   end
   halt();
 end
 
 local function main()
   local t0 = spawn(counter, "counter1", 5)
-  local t1 = spawn(task1)
-  local t2 = spawn(task2)
-  local t3 = spawn(counter, "counter2", 7)
   t0.priority = 1;
+  local t1 = spawn(task1)
   t1.priority = 2;
+  local t2 = spawn(task2)
   t2.priority = 3;
+  local t3 = spawn(counter, "counter2", 7)
   t3.priority = 1;
 
  while (true) do
