190d800ab5dc15768a40945daadb851a2f35f443
coproductions <constantin.dumba@gmail.com>
2016-03-03 11:59:38 -0800

    trying out my scheduler

--- myScheduler.lua
+++ myScheduler.lua
@@ -104,7 +104,7 @@ function Scheduler.step(self)
 
     -- check the priority of the current task and compare it to the next task in the list
   if nextTask then
-    print('in nextTask','task priority: ',task.priority,' nextTask priority: ',task.priority)
+    print('in nextTask','task priority: ',task.priority,' nextTask priority: ',nextTask.priority)
     print('length',self.TasksReadyToRun:length())
     if task.priority < nextTask.priority then
       self.TasksReadyToRun:pushFront(task)
