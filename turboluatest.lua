610c41cf843e6fcd9e46d4a3e5df4fc3a43eae26
coproductions <constantin.dumba@gmail.com>
2016-03-04 16:08:25 -0800

    added turbo lua

---
+++ turboluatest.lua
@@ -0,0 +1,12 @@
+local turbo = require("turbo")
+
+local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
+
+function HelloWorldHandler:get()
+    self:write("Hello World!")
+end
+
+turbo.web.Application({
+    {"/hello", HelloWorldHandler}
+}):listen(8080)
+turbo.ioloop.instance():start()
