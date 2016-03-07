5f3c09cbfdf94e2213552b04f593584f19692033
coproductions <constantin.dumba@gmail.com>
2016-03-06 20:56:29 -0800

    rast

--- turboluatest.lua
+++ turboluatest.lua
@@ -18,26 +18,34 @@ local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
 local latestNumber = "hello from latest"
 local function writeResponse(self,whatever)
     self:write(whatever)
-        yield();
+    yield();
+end
+
+local function task2()
+  print("second task, only line")
+  yield();
 end
 
 function HelloWorldHandler:get()
     writeResponse(self,"hello again")
     self:write("second Response")
     print("got this far")
-  local t0 = spawn(writeResponse, self, "hello again")
-  t0.priority = 2
-  latestNumber = "Michigan"
-  local t1 = spawn(writeResponse, self, "hello against")
-  t1.priority = 2
-  latestNumber = "hello some changes"
-  local t2 = spawn(writeResponse, self, "hello agaisrtnst")
-  t2.priority = 2
-  latestNumber = "have you heard the latest"
-  local t3 = spawn(writeResponse, self, "hello against")
-  t3.priority = 1
+    
+    spawn(task2)
+--   local t0 = spawn(writeResponse, self, "hello again")
+--   t0.priority = 2
+--   latestNumber = "Michigan"
+--   local t1 = spawn(writeResponse, self, "hello against")
+--   t1.priority = 2
+--   latestNumber = "hello some changes"
+--   local t2 = spawn(writeResponse, self, "hello agaisrtnst")
+--   t2.priority = 2
+--   latestNumber = "have you heard the latest"
+--   local t3 = spawn(writeResponse, self, "hello against")
+--   t3.priority = 1
   print("got even further")
-     while (true) do
+   
+   while (true) do
     -- print("priorities: ", t1.priority, t2.priority)
     -- if t1:priority == "dead" and t2:getStatus() == "dead" then
     --   break;
