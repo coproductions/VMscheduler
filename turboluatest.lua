648f86315a72e8c37110f3f67823b043bb18b3a1
coproductions <constantin.dumba@gmail.com>
2016-03-06 20:32:50 -0800

    rast

--- turboluatest.lua
+++ turboluatest.lua
@@ -16,12 +16,12 @@ local HelloWorldHandler = class("HelloWorldHandler", turbo.web.RequestHandler)
 
 
 local latestNumber = "hello from latest"
-local function writeResponse(self)
-    self:write(latestNumber)
+local function writeResponse(self,whatever)
+    self:write(whatever)
 end
 
 function HelloWorldHandler:get()
-    writeResponse(self)
+    writeResponse(self,"hello again")
     self:write("second Response")
     run(main)
 end
@@ -34,16 +34,16 @@ print("listening on 8080")
 turbo.ioloop.instance():start()
 
 local function main()
-  local t0 = spawn(HelloWorldHandler:write, "hello again")
+  local t0 = spawn(writeResponse, HelloWorldHandler, "hello again")
   t0.priority = 2
   latestNumber = "Michigan"
-  local t1 = spawn(HelloWorldHandler:write, "hello against")
+  local t1 = spawn(writeResponse, HelloWorldHandler, "hello against")
   t1.priority = 2
   latestNumber = "hello some changes"
-  local t2 = spawn(HelloWorldHandler:write, "hello agaisrtnst")
+  local t2 = spawn(writeResponse, HelloWorldHandler, "hello agaisrtnst")
   t2.priority = 2
   latestNumber = "have you heard the latest"
-  local t3 = spawn(HelloWorldHandler:write, "hello against")
+  local t3 = spawn(writeResponse, HelloWorldHandler, "hello against")
   t3.priority = 1
      while (true) do
     -- print("priorities: ", t1.priority, t2.priority)
